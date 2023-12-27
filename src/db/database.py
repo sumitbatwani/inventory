04ac2c34e8f7d808752ed79e19290815a6f8f391
sumitkumar23 <sumit@codebase.com>
2023-12-27 15:51:34 +0530

    create index

--- src/db/database.py
+++ src/db/database.py
@@ -4,5 +4,8 @@ client = MongoClient("localhost", 27017)
 
 db = client.inventory
 
+def create_indexes():
+    db.items.create_index({"customerId": 1})
+
 def get_items_collection():
     return db.items
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from db.database import create_indexes
 from fastapi import FastAPI
 from api.items import items_router
 
@@ -6,5 +7,9 @@ app = FastAPI(title="Inventory Management")
 @app.get("/health-check")
 def root():
     return {"status": "OK!"}
+
+@app.on_event("startup")
+def startup():
+    create_indexes()
 
 app.include_router(items_router, prefix="/i/v1", tags=['inventory item'])
